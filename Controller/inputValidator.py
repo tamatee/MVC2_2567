a12d8b3471c5a6cd61431e3a05d14badb72aa677
tamate <teeracharoenkitisap@gmail.com>
2025-02-09 10:13:38 +0700

    create input field and validator

--- Controller/inputValidator.py
+++ Controller/inputValidator.py
@@ -5,44 +5,14 @@ def isValidId(id):
     print(id)
     print(len(id))
     print(not id.startswith('0'))
-    return len(id) == 8 and not (id.startswith('0'))
+    return len(id) == 6 and not (id.startswith('0'))
 
-# วัวต้องมีอายุ 1-10 ปี
-def isValidAge(age):
-    try:
-        regex = age.split('y')
-        regex2 = regex[1].split('m')
-    except:
-        return False
-    return regex[0].isdigit() and 1 <= int(regex[0]) <= 10 and 1 <= int(regex2[0]) <= 12
-# วัวต้องมีเต้านม 4 เต้านม
-def isValidNom(nom):
-    return nom == 4
-
-# วัวตัวนั้นต้องไม่ใช่แพะ
-def isValidSpicie(id):
-    # เรียกจาก csvParser.py หากเป็น Goat จะ return False
-    a = cp.search_cow(id)
-    return a
-
-def validateInput(cowId, cowAge, cowNom):
+def validateInput(suitId):
     isValid = True
     msg = ""
-
-    if not isValidId(cowId):
+    print("Input Validation")
+    if not isValidId(suitId):
         isValid = False
-        msg = "Invalid Cow Id. Please enter a valid Cow ID."
-
-    if not isValidSpicie(cowId):
-        isValid = False
-        msg = "Invalid Specie. Please enter a Cow, not a Goat."
-
-    if not isValidAge(cowAge):
-        isValid = False
-        msg = "Invalid Cow Age. Please enter a valid Cow Age (1-10 years)."
-
-    if not isValidNom(cowNom):
-        isValid = False
-        msg = "Invalid Nom Cnt. Please enter a valid Nom Cnt (4 Nom)."
+        msg = "Invalid Suit Id. Please enter a valid Suit ID."
 
     return isValid, msg
--- View/app.py
+++ View/app.py
@@ -1,69 +1,48 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
 import sys
 from Controller.inputValidator import validateInput
-from Controller.csvParser import parse_csv
-from Controller.milkProcess import milkCow
+# from Controller.csvParser import parse_csv
+# from Controller.milkProcess import milkCow
 
 class MainWindow(QWidget):
 	def __init__(self):
 		super().__init__()
-		self.setWindowTitle("Miking Cow")
+		#Create a window panel
+		self.setWindowTitle("Hero Suit Health Check")
 		self.setGeometry(200, 200, 600, 400)
 
 		self.layout = QVBoxLayout()
 
-		self.cowIdLabel = QLabel("Enter Cow ID:")
-		self.cowAgeLabel = QLabel("Enter Cow Age:")
-		self.cowNomLabel = QLabel("Enter Cow Nom:")
+		#Create labels for the input fields
+		self.suitIdLabel = QLabel("Enter Suit ID:")
+		self.suitIdInput = QLineEdit()
+		self.confirmButton = QPushButton("Confirm")
+		self.confirmButton.clicked.connect(self.confirmButtonClicked)
 
-		self.cowIdInput = QLineEdit()
-		self.cowAgeInput = QLineEdit()
-		self.cowNomInput = QLineEdit()
 
-		# submit button
-		self.submitButton = QPushButton("Submit")
-		self.submitButton.clicked.connect(self.submitButtonClicked)
+		#Apply styling to the labels
+		self.suitIdLabel.setStyleSheet("font-weight: bold; font-size: 14px;")
+		self.suitIdInput.setStyleSheet("background-color: #4CAF50; color: white; font-weight: bold; font-size: 14px; padding: 10px;")
+		self.confirmButton.setStyleSheet("background-color: #4CAF50; color: white; font-weight: bold; font-size: 14px; padding: 10px;")
 
-		# Apply styling to the labels
-		self.cowIdLabel.setStyleSheet("font-weight: bold; font-size: 14px;")
-		self.cowAgeLabel.setStyleSheet("font-weight: bold; font-size: 14px;")
-		self.cowNomLabel.setStyleSheet("font-weight: bold; font-size: 14px;")
+		#Set layout
+		self.layout.addWidget(self.suitIdLabel)
+		self.layout.addWidget(self.suitIdInput)
+		self.layout.addWidget(self.confirmButton)
 
-		# Apply styling to the input fields
-		self.cowIdInput.setStyleSheet("font-size: 14px; height: 30px;")
-		self.cowAgeInput.setStyleSheet("font-size: 14px; height: 30px;")
-		self.cowNomInput.setStyleSheet("font-size: 14px; height: 30px;")
-
-		# Apply styling to the submit button
-		self.submitButton.setStyleSheet("background-color: #4CAF50; color: white; font-weight: bold; font-size: 14px; padding: 10px;")
-
-		self.layout.addWidget(self.cowIdLabel)
-		self.layout.addWidget(self.cowIdInput)
-		self.layout.addWidget(self.cowAgeLabel)
-		self.layout.addWidget(self.cowAgeInput)
-
-		self.layout.addWidget(self.cowNomLabel)
-		self.layout.addWidget(self.cowNomInput)
-
-		self.layout.addWidget(self.submitButton)
-
+		#Render the layout
 		self.setLayout(self.layout)
+		print("this debug message: render success")
 
-	def submitButtonClicked(self):
-		cowId = self.cowIdInput.text()
-		cowAge = self.cowAgeInput.text()
-		cowNom = self.cowNomInput.text()
-
-		isValid, msg = validateInput(cowId, cowAge, cowNom)
+	def confirmButtonClicked(self):
+		suitId = self.suitIdInput.text()
+		isValid, msg = validateInput(suitId)
+		print(f"this debug message: can send to input validator =>{isValid}")
 		if isValid:
-			state = milkCow(cowId, cowAge, cowNom)
-			QMessageBox.about(self, "Milking Status", f"The Cow is Milked.")
+			print("Valid")
+			QMessageBox.about(self, "Suit Status", f"The Suit is in good condition.")
 		else:
 			print(msg)
-
-		# Print the data from the CSV file (ไม่เกี่ยวกับโจทย์)
-		data = parse_csv()
-		print(data)
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
